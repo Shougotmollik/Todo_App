48fa695971594bb7286c3837223b499179a3a183
Shougot mollik <shougotmollik@gmail.com>
2024-06-16 23:37:20 +0600

    Start new project

--- lib/Screens/HomeScreen/home_screen.dart
+++ lib/Screens/HomeScreen/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/Screens/HomeScreen/note_screen.dart';
+import 'package:todo_app/Screens/HomeScreen/profile_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,8 +10,37 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _screens = const [
+    NoteScreen(),
+    ProfileScreen(),
+  ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Todo List",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedIndex,
+          onTap: (index) {
+            _selectedIndex = index;
+            if (mounted) {
+              setState(() {});
+            }
+          },
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.note_alt_sharp), label: "Notes"),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+          ]),
+    );
   }
 }
