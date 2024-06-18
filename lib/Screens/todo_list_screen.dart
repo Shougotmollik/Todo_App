c03c18ef42709dca813d2c2c3e1036880486a412
Shougot mollik <shougotmollik@gmail.com>
2024-06-19 00:23:55 +0600

    todo screen

--- lib/Screens/todo_list_screen.dart
+++ lib/Screens/todo_list_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/entities/todo.dart';
+import 'package:todo_app/widgets/todo_items.dart';
 
 class TodoListScreen extends StatefulWidget {
   const TodoListScreen({super.key});
@@ -8,45 +10,16 @@ class TodoListScreen extends StatefulWidget {
 }
 
 class _TodoListScreenState extends State<TodoListScreen> {
+  List<Todo> _todoList = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Todo List"),
       ),
-      body: _buildItemList(),
+      body: TodoItems(
+        todo: Todo("Title", "description", DateTime.now()),
+      ),
     );
-  }
-
-  ListView _buildItemList() {
-    return ListView.builder(
-      itemCount: 5,
-      itemBuilder: (context, index) {
-        return Card(
-          elevation: 3,
-          child: ListTile(
-            title: const Text("Title Here"),
-            subtitle: const Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text("Description"),
-                Text("Date"),
-              ],
-            ),
-            trailing: _buildIconButton(),
-          ),
-        );
-      },
-    );
-  }
-
-  Widget _buildIconButton() {
-    return CircleAvatar(
-      child: Icon(_getIcon(true)),
-    );
-  }
-
-  IconData _getIcon(bool isDone) {
-    return isDone ? Icons.done : Icons.close;
   }
 }
