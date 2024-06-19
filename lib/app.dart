d025b7b9250c09fb5fd0e69ce73f27b9aae7a451
Shougot mollik <shougotmollik@gmail.com>
2024-06-19 22:56:22 +0600

    implementation

--- lib/app.dart
+++ lib/app.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app/Screens/todo_list_screen.dart';
+import 'package:todo_app/config/theme.dart';
 
 class TodoApp extends StatelessWidget {
   const TodoApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: TodoListScreen(),
+    return MaterialApp(
+      themeMode: ThemeMode.light,
+      theme: AppTheme.lightTheme,
+      home: const TodoListScreen(),
     );
   }
 }
