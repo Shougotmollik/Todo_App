7ea7e77883a7e330d0bd3bc16c85a993a03cacc6
shougotmollik <shougotmollik@gmail.com>
2024-08-14 17:14:07 +0600

    start new project

---
+++ lib/views/screens/auth_screen/splash_screen.dart
@@ -0,0 +1,40 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:live_score_app/views/screens/home_screen/main_nav_bar.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    _moveToNextScreen();
+    super.initState();
+  }
+
+  Future<void> _moveToNextScreen() async {
+    Future.delayed(
+      const Duration(seconds: 2),
+      () {
+        if (mounted) {
+          Get.off(const MainNavBar());
+        }
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.deepOrangeAccent.shade200,
+        body: const Center(
+          child: Text(" APP LOGO"),
+        ));
+  }
+}
