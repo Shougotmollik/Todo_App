e670e87e347a45a05747905618dc4c207680b29a
shougotmollik <shougotmollik@gmail.com>
2024-08-14 17:13:26 +0600

    start new project

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:live_score_app/views/screens/auth_screen/splash_screen.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return const GetMaterialApp(
+      home: SplashScreen(),
+    );
   }
 }
